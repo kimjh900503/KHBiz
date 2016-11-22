9058bba85e10ca8f176c26e51ab6d24fe0d0d4c8
Administrator <Administrator@MSDN-SPECIAL>
2016-11-22 13:57:19 +0900

    start

---
+++ KHBiz/src/main/java/com/khbiz/attend/AttendDAO.java
@@ -0,0 +1,17 @@
+package com.khbiz.attend;
+
+public class AttendDAO {
+	//출근
+	public void checkIn(){
+		
+	}
+	//퇴근
+	public void checkOut(){
+		
+	}
+	//회원가입
+	public void memberIn(){
+		
+	}
+
+}
---
+++ KHBiz/src/main/java/com/khbiz/attend/AttendService.java
@@ -0,0 +1,6 @@
+package com.khbiz.attend;
+
+public class AttendService {
+	//출근
+	//퇴근
+}
